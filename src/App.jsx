95d6ce1fb5be33eb74ac841646a6178f9e526083
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-06-21 19:21:48 -0400

    Perfomed basic page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Mainpage from './pages/MainPage'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+
+    <Route path='/' element={<MainLayout />}>
+
+
+      <Route index element={<Mainpage />} />
+
+
+    </Route>
+
+
+
+  )
+)
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='antialiased'>
+
+      <RouterProvider router = { router } />
+
+    </div>
   )
 }
 
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const MainLayout = () => {
+  return (
+    
+    <>
+    
+        <Outlet />
+    
+    </>
+
+  )
+}
+
+export default MainLayout
